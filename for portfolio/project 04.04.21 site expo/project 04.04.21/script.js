f01d45a21a8f255eb75b561cd3dd1cf083ccfce6
dumasilk_workPC <dumasilk@mail.ru>
2021-05-13 16:12:15 +0300

    Ebus

--- for portfolio/project 04.04.21 site expo/project 04.04.21/script.js
+++ for portfolio/project 04.04.21 site expo/project 04.04.21/script.js
@@ -1,11 +1,7 @@
 let categoriesItem = document.querySelectorAll(".categories__item");
 let tabItem = document.querySelectorAll(".tab_item");
-
-for (let n = 0; n < categoriesItem.length; n++) {
-    categoriesItem[n].addEventListener("click", function() {
-
-        tabItem[n].classList.toggle("show");
+let showClass = document.querySelector(".show");
+let pageWidth = window.screen.width;
 
 
-    })
-}
+// ======================================
