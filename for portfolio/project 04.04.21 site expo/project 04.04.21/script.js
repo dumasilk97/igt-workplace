de98687779c218ecbfa4b5741a083a259c1b6400
dumasilk_workPC <dumasilk@mail.ru>
2021-05-14 14:45:49 +0300

    Tabs, but no animation, btnToTop, but no function

--- for portfolio/project 04.04.21 site expo/project 04.04.21/script.js
+++ for portfolio/project 04.04.21 site expo/project 04.04.21/script.js
@@ -1,22 +1,30 @@
 let categoriesItem = document.querySelectorAll(".categories__item");
+let categoriesName = document.querySelectorAll(".categories__name");
 let tabItem = document.querySelectorAll(".tab_item");
 let showClass = document.querySelector(".show");
 let pageWidth = window.screen.width;
-
-// ======================================
-
-<<<<<<< HEAD
-categoriesItem[1].addEventListener("click", function() {
-    tabItem[1].classList.toggle('show');
-
-})
-=======
+let toTopBtn = document.getElementById("to-top-btn");
 // ======================================
 if (pageWidth < 576) {
-    for (let n = 0; n < categoriesItem.length; n++)
+
+    for (let n = 0; n < categoriesItem.length; n++) {
+
         categoriesItem[n].addEventListener("click", function() {
-            tabItem[n].classList.toggle("show");
+            tabItem[n].classList.toggle('show');
+            event.preventDefault;
 
         })
-}
->>>>>>> 8eeec6d44bf80eaad04b4c7e5075571f1f2364ff
+        categoriesName[n].addEventListener("click", function() {
+            tabItem[n].classList.toggle('show');
+            event.preventDefault;
+
+        })
+    }
+};
+
+
+
+window.onscroll = console.log(pageYOffset);
+
+toTopBtn.style.bottom = '10' + 'px';
+toTopBtn.style.left = '10' + 'px';
