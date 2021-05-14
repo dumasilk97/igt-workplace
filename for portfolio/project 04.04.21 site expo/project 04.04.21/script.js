d3dfb3b7b77453fea86761f1a783c9ffa15a67b4
dumasilk_workPC <dumasilk@mail.ru>
2021-05-14 08:38:03 +0300

    fdfsf

--- for portfolio/project 04.04.21 site expo/project 04.04.21/script.js
+++ for portfolio/project 04.04.21 site expo/project 04.04.21/script.js
@@ -3,5 +3,9 @@ let tabItem = document.querySelectorAll(".tab_item");
 let showClass = document.querySelector(".show");
 let pageWidth = window.screen.width;
 
-
 // ======================================
+
+categoriesItem[1].addEventListener("click", function() {
+    tabItem[1].classList.toggle('show');
+
+})
