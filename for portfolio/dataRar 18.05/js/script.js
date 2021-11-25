cfa84b916c3a0b3f35b25a277c5d1c338d401d9a
dumasilk_workPC <dumasilk@mail.ru>
2021-11-25 16:57:40 +0300

    аываы

--- for portfolio/dataRar 18.05/js/script.js
+++ for portfolio/dataRar 18.05/js/script.js
@@ -1,53 +1,58 @@
 
-    const swiper = new Swiper('.swiper-container', {
-        // Optional parameters
-        direction: 'horizontal',
-        loop: true,
-        slidesPerView: 6,
-        spaceBetween: 21,
-        freeModeMinimumVelocity: 0.1,
-        freeModeMomentumBounceRatio: 0.1,
-        grabCursor: true,
-        preloadImages: true,
-        resistance: true,
-        // Navigation arrows
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
 
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'bullets',
-            clickable: true,
+  const swiper = new Swiper('.swiper1', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 6,
+    spaceBetween: 21,
+    freeModeMinimumVelocity: 0.1,
+    freeModeMomentumBounceRatio: 0.1,
+    preloadImages: true,
+    resistance: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
 
-        },
-      
-        
-      });
+    },
+    
+    
+  });
 
 
-      const reviewsSwiper = new Swiper('.reviews-container',{
-        direction: 'horizontal',
-        loop: true,
-        slidesPerView: 4,
-        spaceBetween: 21,
-        freeModeMinimumVelocity: 0.1,
-        freeModeMomentumBounceRatio: 0.1,
-        grabCursor: true,
-        preloadImages: true,
-        resistance: true,
-        // Navigation arrows
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
 
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'bullets',
-            clickable: true,
 
-        },
 
-      });
+
+  const swiperSlider = new Swiper('.swiper2', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView:4,
+    // centeredSlides: true,
+
+    spaceBetween: 21,
+    freeModeMinimumVelocity: 0.1,
+    freeModeMomentumBounceRatio: 0.1,
+    preloadImages: true,
+    resistance: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination2',
+      type: 'bullets',
+      clickable: true,
+    },
+    
+    
+  });
+
