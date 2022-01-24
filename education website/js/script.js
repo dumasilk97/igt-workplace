6c60f0eeb9217526ea8987994a91bec89d7f3aff
dumasilk97 <dumasilk97@mail.ru>
2022-01-24 07:06:40 +0300

    New project

---
+++ education website/js/script.js
@@ -0,0 +1,31 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    // effect: 'slide', 
+    followFinger: 	true, 
+    // loopedSlides: 4, 
+    slidesPerView: 4,
+   
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
+
+
+  const swiper2 = new Swiper('.swiper2', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    // effect: 'slide', 
+    followFinger: 	true, 
+    // loopedSlides: 4, 
+    slidesPerView: 4.5,
+
+    grabCursor: true,
+   
+    
+  
+  });
