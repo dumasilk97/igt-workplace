e273cfeac6070a0942d6eaba79c4921d87c87bd5
dumasilk97 <dumasilk97@mail.ru>
2022-01-30 21:14:34 +0300

    30.01 - Teachers - done, 2 pages left

--- education website/js/script.js
+++ education website/js/script.js
@@ -44,3 +44,78 @@ const swiper = new Swiper('.swiper', {
     
   
   });
+  const swiper4 = new Swiper('.swiper4', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    // effect: 'slide', 
+    followFinger: 	true, 
+    // loopedSlides: 4, 
+    slidesPerView: 4,
+
+    grabCursor: true,
+   
+    
+  
+  });
+  const swiper5 = new Swiper('.swiper5', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    // effect: 'slide', 
+    followFinger: 	true, 
+    // loopedSlides: 4, 
+    slidesPerView: 4,
+
+    grabCursor: true,
+   
+    
+  
+  });
+  const swiper6 = new Swiper('.swiper6', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    // effect: 'slide', 
+    followFinger: 	true, 
+    // loopedSlides: 4, 
+    slidesPerView: 4,
+
+    grabCursor: true,
+   
+    
+  
+  });
+
+  var firstView = document.getElementsByClassName('Kinder'), count;
+  for (count = 0; count < firstView.length; count++){
+    firstView[count].style.display = 'block';
+  }
+
+
+  function openSchool(evt, schoolName) {
+    // Declare all variables
+    var i, tabcontent, tablist__item, tabId;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablist__item" and remove the class "active"
+    tablist__item = document.getElementsByClassName("tablist__item");
+    for (i = 0; i < tablist__item.length; i++) {
+      tablist__item[i].className = tablist__item[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    
+   
+    tabId = document.getElementsByClassName(schoolName);
+    for (i = 0; i < tabId.length; i++) {
+      tabId[i].style.display = 'block';
+    }
+    evt.currentTarget.className += " active";
+  }
+
